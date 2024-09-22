852761be0b455d53a473670e17aac543e9f57cff
lyflexi <liuyanoutsee@outlook.com>
2024-09-22 15:15:41 +0800

    [a]需求分析提交

---
+++ backend-desensitization/src/main/java/org/lyflexi/backenddesensitization/BackendDesensitizationApplication.java
@@ -0,0 +1,13 @@
+package org.lyflexi.backenddesensitization;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendDesensitizationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BackendDesensitizationApplication.class, args);
+    }
+
+}
---
+++ backend-desensitization/src/test/java/org/lyflexi/backenddesensitization/BackendDesensitizationApplicationTests.java
@@ -0,0 +1,13 @@
+package org.lyflexi.backenddesensitization;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendDesensitizationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
